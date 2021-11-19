2b13a0493b06749f610e475adb5d8d9f77767d6d
Varun Raj <varunrajdec@gmail.com>
2021-11-19 19:17:26 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/osproj_2/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.osproj_2;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:sms/sms.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Test App",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  SmsQuery query = new SmsQuery();
+  List<SmsMessage> allmessages;
+
+  @override
+  void initState() {
+    getAllMessages();
+    super.initState();
+  }
+
+  void getAllMessages() {
+    Future.delayed(Duration.zero, () async {
+      List<SmsMessage> messages = await query.querySms(
+        //querySms is from sms package
+        kinds: [SmsQueryKind.Inbox, SmsQueryKind.Sent, SmsQueryKind.Draft],
+        //filter Inbox, sent or draft messages
+        count: 1000, //number of sms to read
+      );
+
+      setState(() {
+        //update UI
+        allmessages = messages;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("SMS Fetch"),
+          backgroundColor: Colors.deepOrangeAccent,
+          shadowColor: Colors.blueGrey,
+          elevation: 5.0,
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(5),
+            child: allmessages == null
+                ? Center(child: CircularProgressIndicator())
+                : Column(
+                    children: allmessages.map((messageone) {
+                    return Container(
+                      child: Card(
+                          child: ListTile(
+                        leading: Icon(Icons.message_rounded),
+                        title: Padding(
+                            child: Text(messageone.address),
+                            padding: EdgeInsets.only(bottom: 5, top: 5)),
+                        subtitle: Padding(
+                            child: Text(messageone.body),
+                            padding: EdgeInsets.only(bottom: 5, top: 5)),
+                      )),
+                    );
+                  }).toList()),
+          ),
+        ));
+  }
+}
